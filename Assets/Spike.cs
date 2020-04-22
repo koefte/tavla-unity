393372b3655a945cf96595ada96008c0a89b5143
Jakob Pesch <jakob.pesch@rwth-aachen.de>
2020-04-22 13:13:07 +0200

    add unity files

---
+++ Assets/Spike.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    Collider collider;
+    public int pos;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        pos = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Stone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    
+    Vector3 _initialPosition;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+        
+    }
+    private void OnMouseUp()
+    {
+        GameObject spike = GameObject.Find("spike-w");
+        //transform.position = spike.GetComponent<Collider>().bounds.max;
+        transform.position = spike.transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x,newPosition.y);
+    }
+
+    
+}
